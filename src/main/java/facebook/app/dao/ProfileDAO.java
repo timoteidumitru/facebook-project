db6bd76bd1ffd328468299367fcbc55cb2757598
SimidaR <simida15@yahoo.com>
2024-01-04 14:28:25 +0100

    create profile classes

---
+++ src/main/java/facebook/app/dao/ProfileDAO.java
@@ -0,0 +1,15 @@
+package facebook.app.dao;
+
+import facebook.app.entities.Friends;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class ProfileDAO {
+
+}
