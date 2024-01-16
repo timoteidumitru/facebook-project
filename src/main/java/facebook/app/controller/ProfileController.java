45c2091aaaf13b03b631aa7de0b5ba3d1f8cb11d
SimidaR <37744029+SimidaR@users.noreply.github.com>
2024-01-16 21:05:56 +0100

    Update ProfileController.java

--- src/main/java/facebook/app/controller/ProfileController.java
+++ src/main/java/facebook/app/controller/ProfileController.java
@@ -1,48 +1,33 @@
 package facebook.app.controller;
 
 import facebook.app.dao.ProfileDAO;
-import facebook.app.entities.Profile;
+import facebook.app.exceptions.InvalidEmailFormatException;
 
 import java.security.InvalidParameterException;
-import java.util.List;
 
 public class ProfileController {
     private final ProfileDAO profileDAO = new ProfileDAO();
 
-    public void editProfile(int id, String name, String email, int age, String location) {
+    public void createProfile(int id, String name, String email, int age, String location) throws InvalidEmailFormatException {
+        if (name.length() < 2 || location.length() < 2 || email.length() < 2) {
+            throw new InvalidParameterException("The entered data does not correspond to the required size of 3 characters ");
+        }
+        if (!email.contains("@")) {
+            throw new InvalidEmailFormatException("Invalid email format for: " + email);
+        }
+        if (age == 0 || age >= 100) {
+            throw new InvalidParameterException("The age can not be 0 or over 100");
+        }
+        profileDAO.writeProfile(id, name, email, age, location);
+    }
 
+    public void editProfile(int id, String name, String email, int age, String location) {
         if (name.length() < 2 || location.length() < 2) {
-            throw new InvalidParameterException("The data does not have corresponding length");
+            throw new InvalidParameterException("The entered data does not correspond to minimum length");
         }
         if (age == 0 || age >= 110) {
             throw new InvalidParameterException("The age can not be 0 or over 110");
         }
         profileDAO.writeProfile(id, name, email, age, location);
     }
-
-    public List<Profile> getAllProfile() {
-        return profileDAO.readProfile();
-    }
-
-//    public void checkProfile(int currentID) {
-//        List<Profile> profileList = getAllProfile();
-//
-//        for (Profile profile : profileList) {
-//           // if (profileList.get(currentID))
-  
-//                System.out.println("profilul curent" + profile);
-
-//        }
-//        System.out.println("toate profilele" + profileList);
-//    }
-
-//    private boolean isUserUnique(List<Profile> profileList, String email) {
-//        return profileList.stream().noneMatch(profile -> profile.getEmail().equals(email));
-//    }
-
-
-//    public Profile getUserByID(int id) throws UserNotFoundException, UserIOException {
-//        return profileService.getUserByID(id);
-//    }
-
 }
